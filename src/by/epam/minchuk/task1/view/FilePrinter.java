30f256eea9b050ef7c4343a7e7126eb7d0eba4cd
OksanaMinchuk <oks.m89@mail.ru>
2019-03-17 18:15:36 +0300

    fixed printer methods

--- src/by/epam/minchuk/task1/view/FilePrinter.java
+++ src/by/epam/minchuk/task1/view/FilePrinter.java
@@ -7,6 +7,9 @@ public class FilePrinter implements Printable {
     private File fbyte = new File("outputDataByte.data");
     private File fsymb = new File("outputDataSymbol.data");
 
+    private FileOutputStream fileOutputStream = null;
+    private FileWriter fileWriter = null;
+
     @Override
     public void print(Object ob) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(fbyte);
